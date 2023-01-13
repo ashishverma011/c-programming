a6577643934a2b46d9d20772d437117b533d7c83
ashishverma011 <122388483+ashishverma011@users.noreply.github.com>
2023-01-13 14:43:25 +0530

    Create 43. All Operations of String.c

---
+++ 43. All Operations of String.c
@@ -0,0 +1,15 @@
+
+1. strlen: This function returns the length of a string.
+2. strcpy: This function copies one string to another.
+3. strcat: This function concatenates two strings.
+4. strcmp: This function compares two strings and returns an integer based on the result of the comparison.
+5. strchr: This function searches for a particular character in a string and returns a pointer to the first
+occurrence of the character.
+6. strstr: This function searches for a particular substring within a string and returns a pointer to the first
+occurrence of the substring.
+7. strtok: This function breaks a string into tokens based on a specified delimiter.
+8. strspn: This function returns the length of the initial portion of a string that consists only of characters from
+a specified set.
+9. strpbrk: This function searches a string for any of a set of characters and returns a pointer to the first
+occurrence of any of those characters.
+10. strcoll: This function compares two strings using the current locale's collatin
